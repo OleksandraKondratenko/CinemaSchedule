fdcf4636ea70bccab49646a5f17612f0d8e65d1a
Ola <oleksandrakondratenko@gmail.com>
2021-05-07 12:45:37 +0300

    Create Project and Classes

---
+++ Cinema/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema
+{
+    public class Movie
+    {
+        public int TimeMovieInMinuts { get; set; }
+        public string Title { get; set; }
+
+        public Movie(int duratoinFilmInMinuts, string nameFilm)
+        {
+            TimeMovieInMinuts = duratoinFilmInMinuts;
+            Title = nameFilm;
+        }
+    }
+}
---
+++ Cinema/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>()
+            {
+                new Movie(90, "Stranger"),
+                new Movie(60, "Kopitoshka"),
+                new Movie(135, "Ali-Baba")
+            };
+            Schedule schedule = new Schedule(movies, DateTime.Now.Date.Add(new TimeSpan(10, 0, 0)));
+            schedule.CreateSchedule();
+            //schedule.WriteAllLeaves();
+        }
+    }
+}
